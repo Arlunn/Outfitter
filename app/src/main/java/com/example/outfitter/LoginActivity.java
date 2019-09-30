56e183e9e60adb5bf0c6277cf90ab10b52acc4f1
arlunn <ezehat@gmail.com>
2019-09-30 11:17:49 -0400

    intent barebones

--- app/src/main/java/com/example/outfitter/LoginActivity.java
+++ app/src/main/java/com/example/outfitter/LoginActivity.java
@@ -2,28 +2,39 @@ package com.example.outfitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
+    private Button mLoginButton;
+    public final static String EXTRA_MESSAGE = "outfitter.USERNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mTrueButton = (Button) findViewById(R.id.loginButton);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
 
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Go to Profile
+                /*
+                Intent intent = new Intent(this, ProfileActivity.class);
+                EditText username = (EditText) findViewById(R.id.usernameField);
+                String message = username.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                */
             }
+
+
         });
     }
 }
