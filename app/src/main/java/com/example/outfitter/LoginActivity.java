5c51969cbe4a6b25be5de6cc8d3ec5d8c78a758d
arlunn <ezehat@gmail.com>
2019-10-04 17:21:08 -0400

    added onpause

--- app/src/main/java/com/example/outfitter/LoginActivity.java
+++ app/src/main/java/com/example/outfitter/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button mLoginButton;
     public final static String EXTRA_MESSAGE = "outfitter.USERNAME";
+
+    //to turn off logging turn verbose false
+    private static final boolean VERBOSE = true;
+    private static final String TAG = "LoginActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +39,19 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
                 */
             }
-
-
         });
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (VERBOSE) Log.v(TAG, "+ ON RESUME +");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (VERBOSE) Log.v(TAG, "- ON PAUSE -");
     }
 }
