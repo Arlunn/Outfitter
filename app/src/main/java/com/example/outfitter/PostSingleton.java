902155f8814ebfde5ba5925c0442333138a41ad5
arlunn <ezehat@gmail.com>
2019-11-05 21:33:40 -0500

    wrong place in arraylist

--- app/src/main/java/com/example/outfitter/PostSingleton.java
+++ app/src/main/java/com/example/outfitter/PostSingleton.java
@@ -62,8 +62,8 @@ public class PostSingleton {
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+                        posts = new ArrayList<Post>();
                         for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            posts = new ArrayList<Post>();
                             HashMap<String, Object> hash = (HashMap<String, Object>) snapshot.getValue();
                             posts.add(new Post((String)hash.get("user"), (String)hash.get("front"), (HashMap) hash.get("virtualOutfit")));
 
