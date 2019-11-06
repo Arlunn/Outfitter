02b2d03dedd43fa6bbb24cd1edba90e0f0403aec
maestrout <abyars0798@gmail.com>
2019-11-05 21:27:07 -0500

    outfit in feed

--- app/src/main/java/com/example/outfitter/FeedFragment.java
+++ app/src/main/java/com/example/outfitter/FeedFragment.java
@@ -3,9 +3,12 @@ package com.example.outfitter;
 import android.content.Context;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -18,6 +21,7 @@ import androidx.fragment.app.Fragment;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -59,12 +63,58 @@ public class FeedFragment extends Fragment {
             ImageView image = (ImageView) view.findViewById(R.id.postImage);
             Picasso.get().load(img).into(image);
 
+            HashMap<String, String> virtualOutfit = p.virtualOutfit;
+
+            List<String> images = new ArrayList<String>();
+            for (int i = 0; i < virtualOutfit.size(); i++) {
+                images.add(virtualOutfit.get("image_url"+i));
+            }
+
+            GridView grid = (GridView) view.findViewById(R.id.simpleGridView);
+
+
+            MyAdapter adapter = new MyAdapter(getActivity().getApplicationContext(), images);
+
+
+            grid.setAdapter(adapter);
+
             insertPoint.addView(view, j, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
 
 
         }
 
         return v;
+    }
+
+    public class MyAdapter extends ArrayAdapter<String> {
+
+        private List<String> images;
+        private Context context;
+
+        public MyAdapter(Context context,
+                         List<String> images) {
+            super(context,0, images);
+            this.images = images;
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView image = new ImageView(getActivity().getApplicationContext());
+            LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(300,300);
+            layoutParams.gravity= Gravity.CENTER_VERTICAL;
+            image.setLayoutParams(layoutParams);
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            image.setMaxHeight(750);
+            image.setMaxHeight(750);
+            Picasso.get().load(images.get(position)).into(image);
+            return image;
+        }
+
+        public void addImage(String image) {
+            images.add(image);
+            this.notifyDataSetChanged();
+        }
     }
 
 }
