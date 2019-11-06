6fb73746075e8d3cfcd4b97a6f6b5736eeb79d22
abhishekhkumar1912 <46734754+abhishekhkumar1912@users.noreply.github.com>
2019-11-05 19:29:56 -0500

    profile implemented

---
+++ app/src/main/java/com/example/outfitter/AccountSettingActivity.java
@@ -0,0 +1,67 @@
+package com.example.outfitter;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountSettingActivity extends AppCompatActivity implements View.OnClickListener{
+    private String username;
+
+    private final static String USERNAME_PREFERENCE = "name";
+
+    private TextView mUsernameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_accountsettings);
+
+        username = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(USERNAME_PREFERENCE, "username");
+        mUsernameTextView = (TextView) findViewById(R.id.text_profile);
+
+        Button mDeleteAccount = (Button) this.findViewById(R.id.deleteAccountButton);
+        mDeleteAccount.setOnClickListener(this);
+        Button mUpdtePassword = (Button) this.findViewById(R.id.updatePasswordButton);
+        mUpdtePassword.setOnClickListener(this);
+        Button mSignOut = (Button) this.findViewById(R.id.signOutButton);
+        mSignOut.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.deleteAccountButton:
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+                SharedPreferences.Editor editor = settings.edit();
+                editor.remove(USERNAME_PREFERENCE);
+                editor.commit();
+
+                AccountSingleton.get(this).deleteUser(username);
+
+                // Bring up login screen again
+                startActivity(new Intent(this, LoginActivity.class));
+                this.finish();
+                break;
+            case R.id.updatePasswordButton:
+                ((FragmentChangeInterface) this).loadFragment(new UpdatePasswordFragment());
+                break;
+            case R.id.signOutButton:
+                settings = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+                editor = settings.edit();
+                editor.remove(USERNAME_PREFERENCE);
+                editor.commit();
+
+                startActivity(new Intent(this, LoginActivity.class));
+                this.finish();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/outfitter/ProfileFragment.java
+++ app/src/main/java/com/example/outfitter/ProfileFragment.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import static com.example.outfitter.LoginFragment.TAG;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
 
@@ -29,57 +34,79 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, null);
+
         username = PreferenceManager.getDefaultSharedPreferences(getContext()).getString(USERNAME_PREFERENCE, "username");
         mUsernameTextView = (TextView) v.findViewById(R.id.text_profile);
-        mUsernameTextView.setText(username);
+        //mUsernameTextView.setText(username);
+        /**
+         Button mDeleteAccount = (Button) v.findViewById(R.id.deleteAccountButton);
+         mDeleteAccount.setOnClickListener(this);
+         Button mUpdtePassword = (Button) v.findViewById(R.id.updatePasswordButton);
+         mUpdtePassword.setOnClickListener(this);
+         Button mSignOut = (Button) v.findViewById(R.id.signOutButton);
+         mSignOut.setOnClickListener(this);
+         **/
 
-        Button mDeleteAccount = (Button) v.findViewById(R.id.deleteAccountButton);
-        mDeleteAccount.setOnClickListener(this);
-        Button mUpdtePassword = (Button) v.findViewById(R.id.updatePasswordButton);
-        mUpdtePassword.setOnClickListener(this);
-        Button mSignOut = (Button) v.findViewById(R.id.signOutButton);
-        mSignOut.setOnClickListener(this);
-        Button mCloset = (Button) v.findViewById(R.id.closetButton);
-        mCloset.setOnClickListener(this);
-
-
-
+        Button mSetting = (Button) v.findViewById(R.id.settings);
+        mSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AccountSettingActivity.class));
+            }
+        });
 
         return v;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.deleteAccountButton:
-                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-                SharedPreferences.Editor editor = settings.edit();
-                editor.remove(USERNAME_PREFERENCE);
-                editor.commit();
+        /**
+         switch (v.getId()) {
+         case R.id.deleteAccountButton:
+         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+         SharedPreferences.Editor editor = settings.edit();
+         editor.remove(USERNAME_PREFERENCE);
+         editor.commit();
 
-                AccountSingleton.get(getContext()).deleteUser(username);
+         AccountSingleton.get(getContext()).deleteUser(username);
 
-                // Bring up login screen again
-                startActivity(new Intent(getActivity(), LoginActivity.class));
-                getActivity().finish();
-                break;
-            case R.id.updatePasswordButton:
-                ((FragmentChangeInterface) getActivity()).loadFragment(new UpdatePasswordFragment());
-                break;
-            case R.id.signOutButton:
-                settings = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-                editor = settings.edit();
-                editor.remove(USERNAME_PREFERENCE);
-                editor.commit();
+         // Bring up login screen again
+         startActivity(new Intent(getActivity(), LoginActivity.class));
+         getActivity().finish();
+         break;
+         case R.id.updatePasswordButton:
+         ((FragmentChangeInterface) getActivity()).loadFragment(new UpdatePasswordFragment());
+         break;
+         case R.id.signOutButton:
+         settings = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+         editor = settings.edit();
+         editor.remove(USERNAME_PREFERENCE);
+         editor.commit();
 
-                startActivity(new Intent(getActivity(), LoginActivity.class));
-                getActivity().finish();
-                break;
-            case R.id.closetButton:
-                startActivity(new Intent(getActivity(), ClosetFragmentPager.class));
-                break;
-        }
+         startActivity(new Intent(getActivity(), LoginActivity.class));
+         getActivity().finish();
+         break;
+         }
+         **/
     }
+    /**
+     private void setupToolbar(){
+     Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+     setSupportActionBar(toolbar);
 
+     ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+     profileMenu.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+    Log.d(TAG, "onClick: account setting");
+    Intent intent = new Intent(mContext, AccountSettingActivity.class);
+    startActivity(intent);
+    }
+    });
+     }
+
+     private void setSupportActionBar(Toolbar toolbar) {
+     }
+     **/
 
 }
