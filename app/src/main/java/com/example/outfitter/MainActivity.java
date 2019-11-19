a800601afc97ae2b6d4974986d966f357a32661b
maestrout <abyars0798@gmail.com>
2019-11-18 19:00:03 -0500

    fixed fragment loading

--- app/src/main/java/com/example/outfitter/MainActivity.java
+++ app/src/main/java/com/example/outfitter/MainActivity.java
@@ -14,6 +14,10 @@ import androidx.fragment.app.Fragment;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, FragmentChangeInterface {
 
     private final static String USERNAME_PREFERENCE = "name";
+
+    public static final String PREVIOUS_FRAGMENT = "previousFragment";
+    public int prevFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         String username = settings.getString(USERNAME_PREFERENCE, "username");
         AccountSingleton.get(this).updateClothesUris(username);
         OutfitSingleton.get(this).updateOutfitsUris(username);
-        loadFragment(new FeedFragment());
+
+        if (savedInstanceState != null) {
+            prevFragment = savedInstanceState.getInt(PREVIOUS_FRAGMENT, 0);
+        }
+
+        if(prevFragment == 0 || prevFragment == 2) {
+            loadFragment(new PostFragment());
+            prevFragment = 2;
+        }else if(prevFragment ==1){
+            loadFragment(new FeedFragment());
+        }else if(prevFragment ==3){
+            loadFragment(new ProfileFragment());
+        }
         /**Passing each menu ID as a set of Ids because each
         menu should be considered as top level destinations.
 
@@ -34,6 +50,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);**/
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(PREVIOUS_FRAGMENT, prevFragment);
     }
 
     @Override
@@ -54,12 +76,15 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         switch (item.getItemId()){
             case R.id.navigation_feed:
                 fragment = new FeedFragment();
+                prevFragment = 1;
                 break;
             case R.id.navigation_post:
                 fragment = new PostFragment();
+                prevFragment = 2;
                 break;
             case R.id.navigation_profile:
                 fragment = new ProfileFragment();
+                prevFragment = 3;
                 break;
         }
 
